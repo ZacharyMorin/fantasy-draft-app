7f52eb704f2b6de0ef026c187b2e1b24f546f95f
Zach Morin <zmorin15@gmail.com>
2024-07-01 09:51:00 -0400

    WIP

--- src/app/player-table/player-table.component.ts
+++ src/app/player-table/player-table.component.ts
@@ -1,14 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { Player } from '../models/player.model';
 
 @Component({
-  selector: 'app-player-table',
-  standalone: true,
-  imports: [],
-  templateUrl: './player-table.component.html',
-  styleUrl: './player-table.component.css'
+    selector: 'app-player-table',
+    standalone: true,
+    templateUrl: './player-table.component.html',
+    styleUrl: './player-table.component.css',
+    imports: []
 })
-export class PlayerTableComponent {
+export class PlayerTableComponent implements AfterViewInit {
   @Input() players: Player[] = [];
-  
+  @ViewChild("my_modal_1") toggle: ElementRef | undefined;
+
+  ngAfterViewInit(): void {
+    console.log(this.toggle);
+  }
+
+  openModal() {
+    this.toggle?.nativeElement.showModal();
+  }
 }
